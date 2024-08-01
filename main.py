497e7fc3e807bd6e8f9c3a9906d34cef305c02be
PyDev <106985180+pydev0@users.noreply.github.com>
2024-08-01 17:24:40 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from linked_list import LinkedList
+
+my_list = LinkedList()
+my_list.append(2)
+my_list.append(4)
+my_list.append(8)
+my_list.append(16)
+my_list.append(32)
+
+my_list.show()
+my_list.length()
+my_list.push_front(0)
+my_list.show()
+my_list.remove_back()
+my_list.show()
+my_list.remove_front()
+my_list.show()
+my_list.value_at(0)
+my_list.insert(5, 3)
+my_list.show()
+my_list.reverse()
+my_list.show()
